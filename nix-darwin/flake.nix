b0df9d600c2f347f3a667079f3169f81cb3263a1
Will Sturgeon <willstrgn@gmail.com>
2024-02-15 00:40:14 -0500

    Mac laptop on February 15, 2024, at 00:40:14

---
+++ nix-darwin/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "System flake for Mac OS X";
+  inputs = {
+    nix-darwin = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:LnL7/nix-darwin";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { nix-darwin, nixpkgs, self }:
+    let
+      configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = [ pkgs.vim ];
+
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        # nix.package = pkgs.nix;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true; # default shell on catalina
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 4;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "x86_64-darwin";
+      };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#mpb-osx
+      darwinConfigurations.mpb-osx =
+        nix-darwin.lib.darwinSystem { modules = [ configuration ]; };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations.mpb-osx.pkgs;
+    };
+}

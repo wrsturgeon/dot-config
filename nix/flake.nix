05abf6c69c966c48aebd4139f9612e4db2c318bc
Will Sturgeon <willstrgn@gmail.com>
2024-02-26 21:36:53 -0500

    February 26, 2024, at 21:36:51 on Darwin

--- nix/flake.nix
+++ nix/flake.nix
@@ -20,7 +20,7 @@
     };
   };
   outputs = { linux, mac, shared, nix-darwin, nixpkgs, self, }:
-    builtins.trace mac (let
+    builtins.trace "${mac}" (let
       is-linux = nixpkgs.lib.strings.hasSuffix "linux";
       is-mac = nixpkgs.lib.strings.hasSuffix "darwin";
       linux-mac = on-linux: on-mac: system:
@@ -31,18 +31,18 @@
         else
           throw "Unrecognized OS";
       get-username = linux-mac "will" "willsturgeon";
-      config-modules = system: modules: {
-        inherit system;
-        modules = builtins.map (config:
-          config {
-            inherit shared system;
-            username = get-username system;
-          }) (shared ++ modules);
-      };
+      # config-modules = system: modules: {
+      #   inherit system;
+      #   modules = builtins.map (config:
+      #     config {
+      #       inherit shared system;
+      #       username = get-username system;
+      #     }) (shared ++ modules);
+      # };
     in {
-      nixosConfigurations.mbp-nixos =
-        nixpkgs.lib.nixosSystem (config-modules "x86_64-linux" [ linux ]);
-      darwinConfigurations.macbook-macos =
-        nix-darwin.lib.darwinSystem (config-modules "x86_64-darwin" [ mac ]);
+      # nixosConfigurations.mbp-nixos =
+      #   nixpkgs.lib.nixosSystem (config-modules "x86_64-linux" [ linux ]);
+      darwinConfigurations.macbook-macos = builtins.trace mac { };
+      # nix-darwin.lib.darwinSystem (config-modules "x86_64-darwin" [ mac ]);
     });
 }

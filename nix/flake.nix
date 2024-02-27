6c0fd3c8ca3378921d6aa2b04b4e2240731fb21c
Will Sturgeon <willstrgn@gmail.com>
2024-02-26 21:35:16 -0500

    February 26, 2024, at 21:35:13 on Darwin

--- nix/flake.nix
+++ nix/flake.nix
@@ -20,30 +20,29 @@
     };
   };
   outputs = { linux, mac, shared, nix-darwin, nixpkgs, self, }:
-    builtins.trace mac { };
-  # let
-  #   is-linux = nixpkgs.lib.strings.hasSuffix "linux";
-  #   is-mac = nixpkgs.lib.strings.hasSuffix "darwin";
-  #   linux-mac = on-linux: on-mac: system:
-  #     if is-linux system then
-  #       on-linux
-  #     else if is-mac system then
-  #       on-mac
-  #     else
-  #       throw "Unrecognized OS";
-  #   get-username = linux-mac "will" "willsturgeon";
-  #   config-modules = system: modules: {
-  #     inherit system;
-  #     modules = builtins.map (config:
-  #       config {
-  #         inherit shared system;
-  #         username = get-username system;
-  #       }) (shared ++ modules);
-  #   };
-  # in {
-  #   nixosConfigurations.mbp-nixos =
-  #     nixpkgs.lib.nixosSystem (config-modules "x86_64-linux" [ linux ]);
-  #   darwinConfigurations.macbook-macos =
-  #     nix-darwin.lib.darwinSystem (config-modules "x86_64-darwin" [ mac ]);
-  # };
+    builtins.trace mac (let
+      is-linux = nixpkgs.lib.strings.hasSuffix "linux";
+      is-mac = nixpkgs.lib.strings.hasSuffix "darwin";
+      linux-mac = on-linux: on-mac: system:
+        if is-linux system then
+          on-linux
+        else if is-mac system then
+          on-mac
+        else
+          throw "Unrecognized OS";
+      get-username = linux-mac "will" "willsturgeon";
+      config-modules = system: modules: {
+        inherit system;
+        modules = builtins.map (config:
+          config {
+            inherit shared system;
+            username = get-username system;
+          }) (shared ++ modules);
+      };
+    in {
+      nixosConfigurations.mbp-nixos =
+        nixpkgs.lib.nixosSystem (config-modules "x86_64-linux" [ linux ]);
+      darwinConfigurations.macbook-macos =
+        nix-darwin.lib.darwinSystem (config-modules "x86_64-darwin" [ mac ]);
+    });
 }

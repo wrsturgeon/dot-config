f423a26a1b9d5783421237d42d6621bfb23b0649
Will Sturgeon <willstrgn@gmail.com>
2024-02-27 03:18:31 -0500

    February 27, 2024, at 03:18:31 on Darwin

--- nix/os/mac/flake.nix
+++ nix/os/mac/flake.nix
@@ -1,12 +1,13 @@
 {
   description = "MacOS-specific config";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
-  outputs = { nixpkgs, self }: {
+  inputs = { home = { inputs.nixpkgs.follows = "nixpkgs"; url = "path:../home"; }; nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+  outputs = { home, nixpkgs, self }: {
     configure =
-      { linux-mac, nixpkgs-config, shared, stateVersion, system, username }: [{
+      { linux-mac, nixpkgs-config, shared, stateVersion, system, username }@configure-args: [{
         environment = {
           shellAliases.nixos-rebuild =
             "darwin-rebuild --flake .#mbp-macos --keep-going -j auto";
+          packages = (home.configure configure-args).users.${username}.home.packages;
         };
         security.pam.enableSudoTouchIdAuth = true;
         services.nix-daemon.enable = true;

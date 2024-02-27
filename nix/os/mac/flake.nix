6238f9b70e60d16ecd13a341cd3f8923e62b3763
Will Sturgeon <willstrgn@gmail.com>
2024-02-26 21:19:02 -0500

    February 26, 2024, at 21:18:59 on Darwin

--- nix/os/mac/flake.nix
+++ nix/os/mac/flake.nix
@@ -6,7 +6,9 @@
       environment = {
         shellAliases.nixos-rebuild =
           "darwin-rebuild --flake .#macbook-macos --keep-going -j auto";
-        systemPackages = shared.users.users.${username}.packages;
+        systemPackages = (shared {
+          inherit shared system username;
+        }).users.users.${username}.packages;
       };
       nix.linux-builder.enable = true;
       security.pam.enableSudoTouchIdAuth = true;

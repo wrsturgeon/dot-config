c1fed29730596f5456acf7d35f400fdb70feb111
Will Sturgeon <willstrgn@gmail.com>
2024-02-26 21:34:35 -0500

    February 26, 2024, at 21:34:32 on Darwin

--- nix/os/mac/flake.nix
+++ nix/os/mac/flake.nix
@@ -1,17 +1,17 @@
 {
   description = "MacOS-specific config";
   inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
-  outputs = { nixpkgs, self }:
-    { shared, system, username }: {
-      environment = {
-        shellAliases.nixos-rebuild =
-          "darwin-rebuild --flake .#macbook-macos --keep-going -j auto";
-        systemPackages = (shared {
-          inherit shared system username;
-        }).users.users.${username}.packages;
-      };
-      nix.linux-builder.enable = true;
-      security.pam.enableSudoTouchIdAuth = true;
-      services.nix-daemon.enable = true;
-    };
+  outputs = { nixpkgs, self }: { };
+  # { shared, system, username }: {
+  #   environment = {
+  #     shellAliases.nixos-rebuild =
+  #       "darwin-rebuild --flake .#macbook-macos --keep-going -j auto";
+  #     systemPackages = (shared {
+  #       inherit shared system username;
+  #     }).users.users.${username}.packages;
+  #   };
+  #   nix.linux-builder.enable = true;
+  #   security.pam.enableSudoTouchIdAuth = true;
+  #   services.nix-daemon.enable = true;
+  # };
 }

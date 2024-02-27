c820ec2e72d002227fc282f28deed2691da404dd
Will Sturgeon <willstrgn@gmail.com>
2024-02-27 15:36:44 -0500

    February 27, 2024, at 15:36:41 on Linux

--- os/shared/flake.nix
+++ os/shared/flake.nix
@@ -14,7 +14,7 @@
           };
           extraInit = ''
             rm -fr ~/emacs.el ~/.emacs ~/.emacs.d
-            ln -s ~/.config/emacs ~/.emacs.d
+            # ln -s ~/.config/emacs ~/.emacs.d
           '';
           pathsToLink = [ "/share/zsh" ];
           shellAliases = {

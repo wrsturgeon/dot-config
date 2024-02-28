e52b543be242e26806b337a2d4d16e7afc88944b
Will Sturgeon <willstrgn@gmail.com>
2024-02-27 23:43:52 -0500

    February 27, 2024, at 23:43:49 on Darwin

--- home/flake.nix
+++ home/flake.nix
@@ -15,7 +15,7 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     wezterm = {
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:wez/wezterm?dir=nix";
     };
   };

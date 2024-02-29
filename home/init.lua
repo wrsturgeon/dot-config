bde7caed93552f959e2ddb82b546550ead472d3c
Will Sturgeon <willstrgn@gmail.com>
2024-02-28 23:21:13 -0500

    February 28, 2024, at 23:21:10 on Darwin

--- home/init.lua
+++ home/init.lua
@@ -19,9 +19,7 @@ end
 
 -- open a (very small) terminal
 vim.keymap.set('n', '<leader>t', function()
-    local buffer = vim.api.nvim_create_buf(false, false)
-    vim.api.nvim_open_win(buffer, true, { height = 10, split = 'below', win = 0 })
-    vim.api.nvim_open_term(buffer, {})
+    vim.cmd 'split | resize 10 | term'
 end, {})
 
 -- telescope filesystem (<leader>f...)

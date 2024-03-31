dc0d8b1e464442811ead6037e0e9a6358baa5e36
Will Sturgeon <willstrgn@gmail.com>
2024-03-31 14:26:45 -0400

    on Darwin

--- home/flake.nix
+++ home/flake.nix
@@ -195,7 +195,7 @@
                     # if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
                     #   source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
                     # fi
-                    # source ${./p10k.zsh}
+                    source ${./p10k.zsh}
                   '';
                   syntaxHighlighting = {
                     enable = true;

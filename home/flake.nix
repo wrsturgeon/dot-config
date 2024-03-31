0620ffdaa9034f9c529f8b4b58ec09497de58815
Will Sturgeon <willstrgn@gmail.com>
2024-03-31 14:24:19 -0400

    on Darwin

--- home/flake.nix
+++ home/flake.nix
@@ -187,15 +187,15 @@
                     # RSS email update
                     (r2e run &) > /dev/null 2>&1
 
-                    # p10k
-                    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-                    # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-                    # Initialization code that may require console input (password prompts, [y/n]
-                    # confirmations, etc.) must go above this block; everything else may go below.
-                    if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-                      source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-                    fi
-                    source ${./p10k.zsh}
+                    # # p10k
+                    # source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+                    # # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+                    # # Initialization code that may require console input (password prompts, [y/n]
+                    # # confirmations, etc.) must go above this block; everything else may go below.
+                    # if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+                    #   source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+                    # fi
+                    # source ${./p10k.zsh}
                   '';
                   syntaxHighlighting = {
                     enable = true;

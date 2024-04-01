7e4fed01da14b8bd0640df6fd99ce4e012a31053
Will Sturgeon <willstrgn@gmail.com>
2024-04-01 18:00:38 -0400

    avril 1, 2024, at 18:00:35 on Darwin

--- nix/merge.nix
+++ nix/merge.nix
@@ -1,30 +1,34 @@
 let
   merge =
     a:
-    assert builtins.typeOf a == "set";
-    b:
-    assert builtins.typeOf b == "set";
-    let
-      attrs = builtins.attrNames a ++ builtins.attrNames b;
-      print-set = set: builtins.foldl' (acc: x: acc + x + " = ...; ") "{ " (builtins.attrNames set) + "}";
-      case =
-        k:
-        assert builtins.typeOf k == "string";
+    if builtins.typeOf a != "set" then
+      abort "`merge` got a first argument of type `${builtins.typeOf a}` instead of `set`"
+    else
+      b:
+      if builtins.typeOf b != "set" then
+        abort "`merge` got a second argument of type `${builtins.typeOf b}` instead of `set`"
+      else
         let
-          k-in = builtins.hasAttr k;
+          attrs = builtins.attrNames a ++ builtins.attrNames b;
+          print-set = set: builtins.foldl' (acc: x: acc + x + " = ...; ") "{ " (builtins.attrNames set) + "}";
+          case =
+            k:
+            assert builtins.typeOf k == "string";
+            let
+              k-in = builtins.hasAttr k;
+            in
+            if k-in a then
+              if k-in b then merge a.${k} b.${k} else a.${k}
+            else if k-in b then
+              b.${k}
+            else
+              abort "Internal error in `merge` on key `${k}` merging `${print-set a}` and `${print-set b}`";
+          merge-attr = name: {
+            inherit name;
+            value = case name;
+          };
         in
-        if k-in a then
-          if k-in b then merge a.${k} b.${k} else a.${k}
-        else if k-in b then
-          b.${k}
-        else
-          abort "Internal error in `merge` on key `${k}` merging `${print-set a}` and `${print-set b}`";
-      merge-attr = name: {
-        inherit name;
-        value = case name;
-      };
-    in
-    builtins.listToAttrs (builtins.map merge-attr attrs);
+        builtins.listToAttrs (builtins.map merge-attr attrs);
 in
 assert
   merge { a.b = true; } { a.c = false; } == {

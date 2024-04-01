73e27ba8373780f89f82b61244755af4d69b2259
Will Sturgeon <willstrgn@gmail.com>
2024-04-01 17:56:34 -0400

    Fix segmentation fault from stack overflow in `assert`

--- nix/merge.nix
+++ nix/merge.nix
@@ -1,30 +1,36 @@
 let
   merge =
-    a: b:
+    a:
     assert builtins.typeOf a == "set";
+    b:
     assert builtins.typeOf b == "set";
     let
       attrs = builtins.attrNames a ++ builtins.attrNames b;
+      print-set = set: builtins.foldl' (acc: x: acc + x + " = ...; ") "{ " (builtins.attrNames set) + "}";
       case =
-        name:
-        if a ? name then
-          if b ? name then merge a.name b.name else a.name
-        else if b ? name then
-          b.name
+        k:
+        assert builtins.typeOf k == "string";
+        let
+          k-in = builtins.hasAttr k;
+        in
+        if k-in a then
+          if k-in b then merge a.${k} b.${k} else a.${k}
+        else if k-in b then
+          b.${k}
         else
-          abort "Internal error in `merge` on key `${name}`";
+          abort "Internal error in `merge` on key `${k}` merging `${print-set a}` and `${print-set b}`";
       merge-attr = name: {
         inherit name;
         value = case name;
       };
     in
-    assert
-      (merge { a.b = true; } { a.c = false; }) == {
-        a = {
-          b = true;
-          c = false;
-        };
-      };
     builtins.listToAttrs (builtins.map merge-attr attrs);
 in
+assert
+  merge { a.b = true; } { a.c = false; } == {
+    a = {
+      b = true;
+      c = false;
+    };
+  };
 merge

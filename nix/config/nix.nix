7a80ea9394d0e0166e45164f5cfde9b4102c429e
Will Sturgeon <willstrgn@gmail.com>
2024-08-20 11:27:41 -0700

    août 20, 2024, at 11:27:41 on Darwin

--- nix/config/nix.nix
+++ nix/config/nix.nix
@@ -1,5 +1,6 @@
 { linux-mac, nixvim, pkgs, self, system, }: {
-  package = pkgs.nix;
+  # package = pkgs.nix;
+  package = pkgs.nixVersions.latest;
   settings = {
     auto-optimize-store = true;
     experimental-features = [ "nix-command" "flakes" ];

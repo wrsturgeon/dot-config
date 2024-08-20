d3b3f3aab8bb6ba06a669aa04c535e25dc407d55
Will Sturgeon <willstrgn@gmail.com>
2024-08-20 09:59:47 -0700

    août 20, 2024, at 09:59:47 on Darwin

--- nix/config.nix
+++ nix/config.nix
@@ -1,17 +1,18 @@
 {
   linux-mac,
-nixfmt,
+  nixfmt,
   nixvim,
   pkgs,
+  self,
   stateVersion,
   system,
 }:
 {
-environment.systemPackages=(builtins.map (p: p.packages.${system}.default) [nixfmt]);
-nix={package=pkgs.nix;settings.experimental-features="nix-command flakes";};
-nixpkgs.hostPlatform=system;
-programs={zsh.enable=true;};
+  environment.systemPackages=(builtins.map (p: p.packages.${system}.default) [nixfmt]);
+  nix={package=pkgs.nix;settings.experimental-features="nix-command flakes";};
+  nixpkgs.hostPlatform=system;
+  programs={zsh.enable=true;};
   services.nix-daemon.enable = true;
-system={inherit stateVersion;configurationRevision = self.rev or self.dirtyRev or null;
+  system={inherit stateVersion;configurationRevision = self.rev or self.dirtyRev or null;
 };
 }

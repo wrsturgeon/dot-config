bd55a55ca486fde8e010997c1783f4770099a46b
Will Sturgeon <willstrgn@gmail.com>
2024-08-21 12:27:30 -0700

    `2024/08/21 12:27:30 Darwin`

--- nix/config/programs/vim/plugins/lsp.nix
+++ nix/config/programs/vim/plugins/lsp.nix
@@ -14,10 +14,6 @@ ctx: {
     };
   };
   keymaps.lspBuf = {
-    "gd" = "definition";
-    "gD" = "references";
-    "gt" = "type_definition";
-    "gi" = "implementation";
     "K" = "hover";
   };
 }
--- nix/config/programs/vim/plugins/telescope.nix
+++ nix/config/programs/vim/plugins/telescope.nix
@@ -1,12 +1,32 @@
 ctx: {
   keymaps = {
-    "<leader>fg" = "live_grep";
     "<C-p>" = {
       action = "git_files";
       options = {
         desc = "Telescope Git Files";
       };
     };
+    "<leader>fb" = "buffers";
+    "<leader>fc" = "commands";
+    "<leader>ff" = "find_files";
+    "<leader>fg" = "live_grep";
+    "<leader>fk" = "keymaps";
+    "<leader>fm" = "man_pages";
+    "<leader>fo" = "vim_options";
+    "<leader>fr" = "registers";
+    "<leader>ft" = "tags";
+    "<leader>gb" = "git_branches";
+    "<leader>gc" = "git_bcommits";
+    "<leader>gC" = "git_commits";
+    "<leader>gcr" = "git_bcommits_range";
+    "<leader>gs" = "git_status";
+    "<leader>gS" = "git_stash";
+    "gd" = "lsp_implementations";
+    "gc" = "lsp_incoming_calls";
+    "gC" = "lsp_outgoing_calls";
+    "gi" = "lsp_implementations";
+    "gr" = "lsp_references";
+    "gt" = "lsp_implementations";
   };
   extensions = ctx.enable { fzf-native = { }; };
 }

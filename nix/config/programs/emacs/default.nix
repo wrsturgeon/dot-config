127cb7274125f68315d058f232d7ea06a019e247
Will Sturgeon <willstrgn@gmail.com>
2024-08-21 08:58:01 -0700

    août 21, 2024, at 08:58:01 on Darwin

--- nix/config/programs/emacs/default.nix
+++ nix/config/programs/emacs/default.nix
@@ -1,10 +1,9 @@
 ctx:
 let
-  emacs-init = import ./init.el;
   emacs-init-filename = "default.el";
   emacs-init-pkg = ctx.pkgs.runCommand emacs-init-filename { } ''
     mkdir -p $out/share/emacs/site-lisp
-    cp ${ctx.pkgs.writeText emacs-init-filename emacs-init} $out/share/emacs/site-lisp/${emacs-init-filename}
+    cp ${./init.el} $out/share/emacs/site-lisp/${emacs-init-filename}
   '';
   emacs-pkgs =
     epkgs: with epkgs; [

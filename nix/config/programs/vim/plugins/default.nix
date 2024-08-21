7b902b1f2bbba9610d4165d74869cbd293beb4e3
Will Sturgeon <willstrgn@gmail.com>
2024-08-21 11:57:11 -0700

    `2024/08/21 11:57:11 Darwin`

--- nix/config/programs/vim/plugins/default.nix
+++ nix/config/programs/vim/plugins/default.nix
@@ -4,16 +4,11 @@ ctx.enable (
     builtins.map
       (k: {
         name = ctx.pkgs.lib.strings.removeSuffix ".nix" k;
-        value = import "./${k}" ctx;
+        value = import ./${k} ctx;
       })
       (
         builtins.filter (k: k != "default.nix") (
-          let
-            shit = builtins.filter (ctx.pkgs.lib.strings.hasSuffix ".nix") (
-              builtins.attrNames (builtins.readDir ./.)
-            );
-          in
-          builtins.trace (builtins.toString shit) shit
+          builtins.filter (ctx.pkgs.lib.strings.hasSuffix ".nix") (builtins.attrNames (builtins.readDir ./.))
         )
       )
   )

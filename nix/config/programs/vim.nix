acb482161f2518039be4ddb575ffe22f500480e7
Will Sturgeon <willstrgn@gmail.com>
2024-08-21 01:34:35 -0700

    août 21, 2024, at 01:34:35 on Darwin

--- nix/config/programs/vim.nix
+++ nix/config/programs/vim.nix
@@ -3,4 +3,12 @@ let
 in
 {
   colorschemes.${theme}.enable = true;
+  plugins = builtins.mapAttrs (k: v: v // { enable = true; }) {
+    rainbow-delimiters = { };
+    treesitter = {
+      nixGrammars = true;
+      settings.indent.enable = true;
+    };
+    treesitter-context.settings.maxLines = 2;
+  };
 }

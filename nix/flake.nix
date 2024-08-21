a9b4cb26a393f3e1a65b6fbb044fb56258957d79
Will Sturgeon <willstrgn@gmail.com>
2024-08-21 01:09:39 -0700

    août 21, 2024, at 01:09:39 on Darwin

--- nix/flake.nix
+++ nix/flake.nix
@@ -64,7 +64,7 @@
         username = linux-mac "will" "willsturgeon";
 
         # Vim
-        vim = (builtins.trace (builtins.attrNames nixvim) nixvim).packages.${system}.makeNixvim { };
+        vim = (builtins.trace (builtins.attrNames nixvim.lib) nixvim).packages.${system}.makeNixvim { };
 
         # Emacs
         emacs-init = ''

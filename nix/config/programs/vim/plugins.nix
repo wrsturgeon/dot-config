790da9774dd09961ebd5383307dceeef04da88a8
Will Sturgeon <willstrgn@gmail.com>
2024-08-21 11:22:50 -0700

    août 21, 2024, at 11:22:50 on Darwin

--- nix/config/programs/vim/plugins.nix
+++ nix/config/programs/vim/plugins.nix
@@ -30,7 +30,18 @@ ctx.enable {
   lualine = { };
   rainbow-delimiters = { };
   # rust-tools = { };
-  telescope = { };
+  telescope = {
+    keymaps = {
+      "<leader>fg" = "live_grep";
+      "<C-p>" = {
+        action = "git_files";
+        options = {
+          desc = "Telescope Git Files";
+        };
+      };
+    };
+    extensions = ctx.enable { fzf-native = { }; };
+  };
   treesitter = {
     nixGrammars = true;
     settings.indent.enable = true;

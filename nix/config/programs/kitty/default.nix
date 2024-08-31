2bc3fe1c37bbe807c07e795a37825018820c6c2d
Will Sturgeon <willstrgn@gmail.com>
2024-08-30 20:02:53 -0700

    `2024/08/30 20:02:53 Darwin`

--- nix/config/programs/kitty/default.nix
+++ nix/config/programs/kitty/default.nix
@@ -1,16 +1,20 @@
 ctx:
 ctx.pkgs.writeTextFile {
   destination = "/kitty.conf";
-  name = "kitty.conf";
+  name = "kitty-config";
   text = ''
     include ${ctx.pkgs.kitty-themes}/share/kitty-themes/themes/GitHub_Dark.conf
 
-    font_family Iosevka Custom Light
+    # font_family Iosevka Custom Light
+    font_family Iosevka Custom Regular
     bold_font Iosevka Custom Extrabold
-    italic_font Iosevka Custom Light Italic
+    # italic_font Iosevka Custom Light Italic
+    italic_font Iosevka Custom Regular Italic
     bold_italic_font Iosevka Custom Extrabold Italic
 
     font_size 13
+
+    symbol_map Iosevka Custom Regular
 
     disable_ligatures always
   '';

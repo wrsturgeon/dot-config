1dc83d9a80db8b1c845ceb311335cf9c53af63d7
Will Sturgeon <willstrgn@gmail.com>
2024-08-30 17:06:24 -0700

    `2024/08/30 17:06:24 Darwin`

--- nix/flake.nix
+++ nix/flake.nix
@@ -111,30 +111,30 @@
             discord
             slack
             arc-browser
-            # (kitty.override {
-            #   python3Packages = pkgs.python3Packages // {
-            #     buildPythonApplication =
-            #       cfg:
-            #       pkgs.python3Packages.buildPythonApplication (
-            #         cfg
-            #         // {
-            #           configurePhase = ''
-            #             ${cfg.configurePhase}
-            #             sed -i 's/raise SystemExit.*font.*was not found on your system, please install it.*/return/g' setup.py
-            #           '';
-            #           installPhase = ''
-            #             ${cfg.installPhase}
-            #             cp ${iosevka}/share/fonts/truetype/Iosevkacustom-Regular.ttf ./fonts/SymbolsNerdFontMono-Regular.ttf
-            #           '';
-            #           nativeBuildInputs =
-            #             cfg.nativeBuildInputs
-            #             ++ (with pkgs.python3Packages; [
-            #               matplotlib
-            #             ]);
-            #         }
-            #       );
-            #   };
-            # })
+            (kitty.override {
+              python3Packages = pkgs.python3Packages // {
+                buildPythonApplication =
+                  cfg:
+                  pkgs.python3Packages.buildPythonApplication (
+                    cfg
+                    // {
+                      configurePhase = ''
+                        ${cfg.configurePhase}
+                        sed -i 's/raise SystemExit.*font.*was not found on your system, please install it.*/return/g' setup.py
+                      '';
+                      installPhase = ''
+                        ${cfg.installPhase}
+                        cp ${iosevka}/share/fonts/truetype/Iosevkacustom-Regular.ttf ./fonts/SymbolsNerdFontMono-Regular.ttf
+                      '';
+                      nativeBuildInputs =
+                        cfg.nativeBuildInputs
+                        ++ (with pkgs.python3Packages; [
+                          matplotlib
+                        ]);
+                    }
+                  );
+              };
+            })
             # wezterm
             logseq
           ];

f069d128e2b375c77e0cad97f75ccab4772b53db
Will Sturgeon <willstrgn@gmail.com>
2024-09-01 03:24:35 -0700

    `2024/09/01 03:24:35 Darwin`

--- nix/config/programs/neovim/default.nix
+++ nix/config/programs/neovim/default.nix
@@ -4,7 +4,8 @@ let
   enable = builtins.mapAttrs (k: v: v // { enable = true; });
 in
 {
-  colorschemes.${theme}.enable = true;
+  # colorschemes.${theme}.enable = true;
+  colorscheme = theme;
   extraPlugins = (with ctx; [ github-dark-nvim ]) ++ (with ctx.pkgs.vimPlugins; [ Coqtail ]);
   opts = import ./options.nix;
   plugins = import ./plugins (ctx // { inherit enable; });

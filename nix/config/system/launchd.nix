7a919af03d6dee028e80e9a23f1942963f22bf00
Will Sturgeon <willstrgn@gmail.com>
2024-09-02 22:42:53 -0700

    `2024/09/02 22:42:53 Darwin`

--- nix/config/system/launchd.nix
+++ nix/config/system/launchd.nix
@@ -1,4 +1,25 @@
-ctx: {
+ctx:
+let
+  daemons = builtins.mapAttrs (
+    k: v:
+    v
+    // {
+      serviceConfig = v.serviceConfig // {
+        StandardOutPath = "/var/log/${k}.out.log";
+        StandardErrorPath = "/var/log/${k}.err.log";
+      };
+    }
+  );
+in
+{
+  daemons = daemons {
+    custom-system-update = {
+      script = ''
+        cd ~/.config/nix
+        ./rebuild
+      '';
+    };
+  };
   envVariables = {
     LANG = "fr_FR.UTF-8";
   };

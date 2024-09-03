38743fc96e524b5fc8b31514be0a9c518ad90390
Will Sturgeon <willstrgn@gmail.com>
2024-09-03 14:11:28 -0400

    `2024/09/03 14:11:26 Linux `:wrench:

--- config/system/programs.nix
+++ config/system/programs.nix
@@ -6,11 +6,10 @@ builtins.mapAttrs (k: v: v // { enable = true; }) {
     nix-direnv.enable = true;
   };
   tmux = {
-    enableFzf = true;
     enableMouse = true;
     enableSensible = true;
     enableVim = true;
-  };
+  } // ctx.linux-mac { } { enableFzf = true; };
   zsh = {
     enableBashCompletion = true;
     enableCompletion = true;

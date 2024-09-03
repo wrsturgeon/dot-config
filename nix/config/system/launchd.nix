fe15abfd00675c69f19acea03d985101e9f51147
Will Sturgeon <willstrgn@gmail.com>
2024-09-03 00:52:02 -0700

    `2024/09/03 00:52:02 Darwin`

--- nix/config/system/launchd.nix
+++ nix/config/system/launchd.nix
@@ -2,16 +2,14 @@ ctx:
 let
   with-service-config = builtins.mapAttrs (
     k: v:
-    let
-      overwritten = v // {
-        serviceConfig = (v.serviceConfig or { }) // {
-          KeepAlive = true;
-          StandardOutPath = "/var/log/${k}.out.log";
-          StandardErrorPath = "/var/log/${k}.err.log";
-        };
+    v
+    // {
+      serviceConfig = (v.serviceConfig or { }) // {
+        KeepAlive = true;
+        StandardOutPath = "/var/log/${k}.out.log";
+        StandardErrorPath = "/var/log/${k}.err.log";
       };
-    in
-    builtins.trace (ctx.print { ${k} = overwritten; }) overwritten
+    }
   );
 in
 {

f5418cac302cfe17b92a784ca84046f727b36283
Will Sturgeon <willstrgn@gmail.com>
2024-09-03 12:23:21 -0400

    `2024/09/03 12:23:20 Linux `:wrench:

--- config/programs/emacs/default.nix
+++ config/programs/emacs/default.nix
@@ -5,14 +5,7 @@ let
     mkdir -p $out/share/emacs/site-lisp
     cp ${./init.el} $out/share/emacs/site-lisp/${emacs-init-filename}
   '';
-  emacs-pkgs =
-    epkgs: with epkgs; [
-      evil
-      material-theme
-      proof-general
-    ];
+  emacs-pkgs = epkgs: with epkgs; [ evil material-theme proof-general ];
   raw-emacs = ctx.pkgs.emacs-nox; # ctx.pkgs.emacs;
-in
-(ctx.pkgs.emacsPackagesFor raw-emacs).emacsWithPackages (
-  epkgs: [ emacs-init-pkg ] ++ (emacs-pkgs epkgs)
-)
+in (ctx.pkgs.emacsPackagesFor raw-emacs).emacsWithPackages
+(epkgs: [ emacs-init-pkg ] ++ (emacs-pkgs epkgs))

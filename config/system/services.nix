5a7c9ee89154f5509ab55232fbad85483384f95d
Will Sturgeon <willstrgn@gmail.com>
2024-09-03 15:10:51 -0400

    `2024/09/03 15:10:46 Linux `:wrench:

--- config/system/services.nix
+++ config/system/services.nix
@@ -1,16 +1,16 @@
 ctx:
-builtins.mapAttrs (_: s: s // { enable = true; }) ({
+(builtins.mapAttrs (_: s: s // { enable = true; }) ({
   emacs = {
     package = ctx.emacs;
     # exec = "emacs";
   };
 } // (ctx.linux-mac {
   openssh = { };
-  pantheon.apps.enable = false;
   xserver = {
     desktopManager.pantheon.enable = true;
     displayManager.lightdm.enable = true;
     xkb.layout = "us";
     libinput.enable = true;
   };
-} { nix-daemon = { }; }))
+} { nix-daemon = { }; })))
+// ctx.linux-mac { pantheon.apps.enable = false; } { }

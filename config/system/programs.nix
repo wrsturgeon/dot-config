9a360b2f29a7e75f742a0db032ef346881b2a8b7
Will Sturgeon <willstrgn@gmail.com>
2024-09-03 14:15:24 -0400

    `2024/09/03 14:15:22 Linux `:wrench:

--- config/system/programs.nix
+++ config/system/programs.nix
@@ -8,12 +8,14 @@ builtins.mapAttrs (k: v: v // { enable = true; }) ({
   zsh = {
     enableBashCompletion = true;
     enableCompletion = true;
-    enableFzfGit = true;
-    enableFzfHistory = true;
     enableSyntaxHighlighting = true;
     promptInit =
       "source ${ctx.pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-  } // ctx.linux-mac { } { enableFzfCompletion = true; };
+  } // ctx.linux-mac { } {
+    enableFzfCompletion = true;
+    enableFzfGit = true;
+    enableFzfHistory = true;
+  };
 } // ctx.linux-mac { } {
   tmux = {
     enableFzf = true;

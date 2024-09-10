f253f35724145451fd4121d20082d9c57c9ce859
Will Sturgeon <willstrgn@gmail.com>
2024-09-09 19:51:43 -0700

    `2024/09/09 19:51:39 `:apple:` `:wrench:` `willsturgeon

--- config/programs/wezterm/default.nix
+++ config/programs/wezterm/default.nix
@@ -5,5 +5,13 @@ ctx.pkgs.stdenvNoCC.mkDerivation {
   buildPhase = ":";
   installPhase = ''
     cp -r ${ctx.pkgs.wezterm} $out
+    for f in $(find $out -name 'wezterm'); do
+      mv $f $f-raw
+      echo "${''
+        #!/usr/bin/env bash
+
+        $f --config-file ${./config.lua}
+      ''}" > $f
+    done
   '';
 }

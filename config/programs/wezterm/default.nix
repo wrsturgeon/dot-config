9c86a7eb2dc26063bd6214178cd718fe53a9f0ac
Will Sturgeon <willstrgn@gmail.com>
2024-09-09 20:49:14 -0700

    `2024/09/09 20:49:10 `:apple:` `:wrench:` `willsturgeon

--- config/programs/wezterm/default.nix
+++ config/programs/wezterm/default.nix
@@ -2,18 +2,26 @@ ctx:
 ctx.pkgs.stdenvNoCC.mkDerivation {
   name = "wezterm-configured";
   src = ctx.pkgs.wezterm;
+  # buildPhase = ''
+  #   for f in $(find . -type f); do
+  #     if [ -x "$f" ]; then
+  #       mv "$f" "$f-raw"
+  #       echo "${''
+  #         #!/usr/bin/env bash
+  #
+  #         $out/$f --config-file ${./config.lua}
+  #       ''}" > "$f"
+  #       chmod +x "$f"
+  #     fi
+  #   done
+  # '';
   buildPhase = ''
-    for f in $(find . -type f); do
-      if [ -x "$f" ]; then
-        mv "$f" "$f-raw"
-        echo "${''
-          #!/usr/bin/env bash
+    mv bin/wezterm-gui bin/wezterm-gui-raw
+    echo "${''
+      #!/usr/bin/env bash
 
-          $out/$f --config-file ${./config.lua}
-        ''}" > "$f"
-        chmod +x "$f"
-      fi
-    done
+      $out/bin/wezterm-gui-raw --config-file ${./config.lua}
+    ''}" > bin/wezterm-gui
   '';
   installPhase = "cp -r . $out";
 }

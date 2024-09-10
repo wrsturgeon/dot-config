f6b416b9f3ba518283c2a278eeaded188e221215
Will Sturgeon <willstrgn@gmail.com>
2024-09-10 15:42:14 -0700

    `2024/09/10 15:42:09`    :apple:    :wrench:    willsturgeon

--- config/system/nix.nix
+++ config/system/nix.nix
@@ -1,6 +1,15 @@
 ctx: {
   channel.enable = false;
   optimise.automatic = true; # <https://discourse.nixos.org/t/difference-between-nix-settings-auto-optimise-store-and-nix-optimise-automatic/25350>
+  gc = {
+    automatic = true;
+    interval = {
+      Weekday = 0;
+      Hour = 0;
+      Minute = 0;
+    };
+    options = "--delete-older-than 30d";
+  };
   settings = {
     # auto-optimise-store = true;
     experimental-features = [

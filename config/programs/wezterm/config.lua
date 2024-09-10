25f8780b1dfe40f37df16e808c76c94426b03a93
Will Sturgeon <willstrgn@gmail.com>
2024-09-09 21:37:22 -0700

    `2024/09/09 21:37:18 `:apple:` `:wrench:` `willsturgeon

--- config/programs/wezterm/config.lua
+++ config/programs/wezterm/config.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 'Ayu'
+config.color_scheme = 'Ayu Dark (Gogh)'
 config.font = wezterm.font 'Iosevka Custom'
 config.front_end = "WebGpu"
 

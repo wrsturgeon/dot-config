684e4fbcd4f661b2de47daf8d0bbd6b3fa57672b
Will Sturgeon <willstrgn@gmail.com>
2024-09-09 23:14:57 -0700

    `2024/09/09 23:14:53 `:apple:` `:wrench:` `willsturgeon

--- config/programs/wezterm/config.lua
+++ config/programs/wezterm/config.lua
@@ -5,7 +5,7 @@ config.front_end = "WebGpu"
 
 config.color_scheme = 'ayu' -- 'Ayu Dark (Gogh)'
 config.font = wezterm.font 'Iosevka Custom'
-config.font_size = 13
+config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 
 return config

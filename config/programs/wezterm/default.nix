e80ab63413a5b4ca68bf0225e113f18549a57f6a
Will Sturgeon <willstrgn@gmail.com>
2024-09-11 18:55:56 -0700

    `2024/09/11 18:55:53`    :apple:    :wrench:    willsturgeon

--- config/programs/wezterm/default.nix
+++ config/programs/wezterm/default.nix
@@ -17,11 +17,13 @@ ctx.pkgs.stdenvNoCC.mkDerivation {
     ${ctx.pkgs.tree}/bin/tree .
     mkdir -p $out/Applications
     cp -r WezTerm.app $out/Applications/WezTerm.app
-    mkdir -p $out/bin
-    echo "${''
-      #!/usr/bin/env bash
-
-      $out/bin/wezterm-gui-raw --config-file ${./config.lua}
-    ''}" > bin/wezterm-gui
+    ln -s $out/Applications/WezTerm.app/Contents/MacOS $out/bin
+    for f in $(find $out -name 'wezterm-gui'); do
+      mv $f ''${f}-raw
+      echo "${''
+        #!/usr/bin/env bash
+        ''${f}-raw --config-file ${./config.lua}
+      ''}" > $f
+    done
   '';
 }

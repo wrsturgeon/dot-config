38ad844b84727fbb542d444057beaa8167cd33c3
Will Sturgeon <willstrgn@gmail.com>
2024-09-12 06:11:36 +0000

    Wezterm edits from Linux

--- config/programs/wezterm/default.nix
+++ config/programs/wezterm/default.nix
@@ -3,21 +3,12 @@ ctx.pkgs.stdenvNoCC.mkDerivation {
   name = "wezterm-configured";
   src = ctx.linux-mac ctx.pkgs.wezterm ctx.wezterm-zip;
   buildPhase = ":";
-  # buildPhase = ''
-  #   mv bin/wezterm-gui bin/wezterm-gui-raw
-  #   echo "${''
-  #     #!/usr/bin/env bash
-  #
-  #     $out/bin/wezterm-gui-raw --config-file ${./config.lua}
-  #   ''}" > bin/wezterm-gui
-  #   chmod +x bin/wezterm-gui
-  # '';
-  # installPhase = "cp -r . $out";
   installPhase = ''
-    ${ctx.pkgs.tree}/bin/tree .
-    mkdir -p $out/Applications
-    cp -r WezTerm.app $out/Applications/WezTerm.app
-    ln -s $out/Applications/WezTerm.app/Contents/MacOS $out/bin
+    ${ctx.linux-mac "" ''
+      mkdir -p $out/Applications
+      cp -r WezTerm.app $out/Applications/WezTerm.app
+      ln -s $out/Applications/WezTerm.app/Contents/MacOS $out/bin
+    ''}
     for f in $(find $out -name 'wezterm-gui'); do
       mv $f ''${f}-raw
       echo "${''

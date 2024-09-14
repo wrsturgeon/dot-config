45beb7795ac5c6fed30b19204fd050110a9251fb
Will Sturgeon <willstrgn@gmail.com>
2024-09-13 20:57:24 -0700

    `2024/09/13 20:57:20`    :apple:    :wrench:    root

--- config/system/homebrew.nix
+++ config/system/homebrew.nix
@@ -3,10 +3,10 @@ ctx.linux-mac null {
   # Effectively disallow Homebrew by immediately "zapping" everything installed out of existence:
   brews = [ ];
   casks = [
-    "minecraft"
-    "signal"
-    "steam"
-    "tor-browser"
+    # "minecraft"
+    # "signal"
+    # "steam"
+    # "tor-browser"
   ];
   enable = true;
   masApps = { };

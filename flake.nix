6ad63502ff2479d1ef57ab81eeabe2aa9985df5f
Will Sturgeon <willstrgn@gmail.com>
2024-09-15 14:16:05 -0700

    `2024/09/15 14:15:58` :apple: :wrench:

--- flake.nix
+++ flake.nix
@@ -21,11 +21,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim = {
       inputs = {
+        home-manager.follows = "null";
         nix-darwin.follows = "nix-darwin";
         nixpkgs.follows = "nixpkgs";
       };
       url = "github:nix-community/nixvim";
     };
+    null.url = "./null";
     wezterm-zip = {
       flake = false;
       url = "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-macos-nightly.zip";
@@ -40,6 +42,7 @@
       nixos-hardware,
       nixpkgs,
       nixvim,
+      null,
       self,
       wezterm-zip,
     }:

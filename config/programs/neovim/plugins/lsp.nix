56c895c3fded54943e84c56ff9ebbec984d8860c
Will Sturgeon <willstrgn@gmail.com>
2024-10-21 10:44:50 -0700

    `2024/10/21 10:44:44` :apple: :wrench:

--- config/programs/neovim/plugins/lsp.nix
+++ config/programs/neovim/plugins/lsp.nix
@@ -2,11 +2,14 @@ ctx: {
   servers = ctx.enable {
     bashls = { };
     clangd = { };
-    hls = { };
+    hls = {
+      installGhc = true;
+      ghcPackage = ctx.pkgs.ghc;
+    };
     nixd = { };
     ocamllsp.package = ctx.coq.ocamlPackages.ocaml-lsp;
     ruff = { };
-    rust-analyzer = {
+    rust_analyzer = {
       installCargo = true;
       cargoPackage = ctx.rust.cargo;
       installRustc = true;

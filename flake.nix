629d198060ad725adb5f96b2f8545819f4dd2236
Will Sturgeon <willstrgn@gmail.com>
2024-11-09 23:07:31 -0800

    `2024/11/09 23:07:26` :apple: :wrench:

--- flake.nix
+++ flake.nix
@@ -3,7 +3,13 @@
   inputs = {
     fenix = {
       url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-analyzer-src = {
+          url = "github:rust-lang/rust-analyzer/nightly";
+          flake = false;
+        };
+      };
     };
     flake-utils.url = "github:numtide/flake-utils";
     github-dark-nvim-src = {
@@ -239,7 +245,12 @@
           hardware-configuration = import config/hardware-configuration.nix;
           dir = ./config;
           rebuild = import ./rebuild.nix (cfg-args laptop-name);
-          rust = fenix.packages.${system}.minimal;
+          rust = fenix.packages.${system}.default.withComponents [
+            "cargo"
+            "clippy"
+            "rustc"
+            "rustfmt"
+          ];
           vim = nixvim.legacyPackages.${system}.makeNixvim (
             import config/programs/neovim (cfg-args laptop-name)
           );
